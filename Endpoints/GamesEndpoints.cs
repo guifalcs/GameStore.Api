5af4a5b07353d8f926f2f3da27330ea542b18602
Guilherme Falcão <guilherme.falcao@unvoid.com>
2025-06-12 20:32:52 -0300

    Creating a class to the endpoints and a group router

---
+++ Endpoints/GamesEndpoints.cs
@@ -0,0 +1,71 @@
+using Gamestore.Api.Dtos;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GameEndpoints
+{
+    private static readonly List<GameDto> games = [
+    new (1, "The Legend of Zelda: Breath of the Wild", "Action-adventure", 59.99m, new DateOnly(2017, 3, 3)),
+        new (2, "Super Mario Odyssey", "Platformer", 59.99m, new DateOnly(2017, 10, 27)),
+        new (3, "Animal Crossing: New Horizons", "Social simulation", 59.99m, new DateOnly(2020, 3, 20)),
+    ];
+
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("games");
+
+        group.MapGet("/", () => games);
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            var game = games.Find(game => game.Id == id);
+
+            return game is null ? Results.NotFound() : Results.Ok(game);
+
+        })
+            .WithName("GetGameById");
+
+        group.MapPost("/", (CreateGameDto newGame) =>
+        {
+            GameDto game = new GameDto(
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre,
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            games.Add(game);
+
+            return Results.CreatedAtRoute("getGameById", new { id = game.Id }, game);
+        });
+
+        group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            games[index] = new GameDto(
+                id,
+                updatedGame.Name,
+                updatedGame.Genre,
+                updatedGame.Price,
+                updatedGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.Id == id);
+            return Results.NoContent();
+        });
+
+        return group;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,64 +1,8 @@
-using Gamestore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<GameDto> games = [
-    new (1, "The Legend of Zelda: Breath of the Wild", "Action-adventure", 59.99m, new DateOnly(2017, 3, 3)),
-    new (2, "Super Mario Odyssey", "Platformer", 59.99m, new DateOnly(2017, 10, 27)),
-    new (3, "Animal Crossing: New Horizons", "Social simulation", 59.99m, new DateOnly(2020, 3, 20)),
-];
-
-app.MapGet("games", () => games);
-
-app.MapGet("games/{id}", (int id) =>
-{
-    var game = games.Find(game => game.Id == id);
-
-    return game is null ? Results.NotFound() : Results.Ok(game);
-
-})
-    .WithName("GetGameById");
-
-app.MapPost("games", (CreateGameDto newGame) =>
-{
-    GameDto game = new GameDto(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-
-    games.Add(game);
-
-    return Results.CreatedAtRoute("getGameById", new { id = game.Id }, game);
-});
-
-app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-
-    if (index == -1)
-    {
-        return Results.NotFound();
-    }
-
-    games[index] = new GameDto(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-app.MapDelete("games/{id}", (int id) =>
-{
-    games.RemoveAll(game => game.Id == id);
-    return Results.NoContent();
-});
+app.MapGamesEndpoints();
 
 app.Run();
