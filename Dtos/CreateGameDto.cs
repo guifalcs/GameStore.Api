bc8e2d5984f55ce2a548bbb60b4bfb8b45a01f54
Guilherme Falcão <guilherme.falcao@unvoid.com>
2025-06-12 20:01:32 -0300

    practicing CRUD

---
+++ Dtos/CreateGameDto.cs
@@ -0,0 +1,3 @@
+namespace Gamestore.Api.Dtos;
+
+public record class CreateGameDto(string Name, string Genre, decimal Price, DateOnly ReleaseDate);
--- Program.cs
+++ Program.cs
@@ -1,6 +1,47 @@
+using Gamestore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (1, "The Legend of Zelda: Breath of the Wild", "Action-adventure", 59.99m, new DateOnly(2017, 3, 3)),
+    new (2, "Super Mario Odyssey", "Platformer", 59.99m, new DateOnly(2017, 10, 27)),
+    new (3, "Animal Crossing: New Horizons", "Social simulation", 59.99m, new DateOnly(2020, 3, 20)),
+];
+
+app.MapGet("games", () => games);
+
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+    .WithName("GetGameById");
+
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game = new GameDto(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute("getGameById", new { id = game.Id }, game);
+});
+
+app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var index = games.FindIndex(game => game.Id == id);
+
+    games[index] = new GameDto(
+        id,
+        updatedGame.Name,
+        updatedGame.Genre,
+        updatedGame.Price,
+        updatedGame.ReleaseDate
+    );
+
+    return Results.NoContent();
+});
 
 app.Run();
