cfb1e1f07e506af9291cf389f0e0ebea3334effb
Guilherme Falcão <guilherme.falcao@unvoid.com>
2025-06-14 14:29:14 -0300

    create db connection

--- obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebda53ead4bd143bbaeed47de79065e7a4752589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd57aaadd2d7e1fd760ca6cef12eb8c35c97e9b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
